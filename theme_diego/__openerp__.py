78313753456bf5c925a8cc469648ee95136a5623
dabalrodriguez <dabalrodriguez@atanasoft23.danielcastelao.org>
2016-02-24 10:28:24 +0100

    Mi tema de Odoo

---
+++ theme_diego/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Theme_diego',
+    'description': 'Default website theme to showcase customization possibilities.',
+    'category': 'Theme/Hidden',
+    'sequence': 1000,
+    'version': '1.0',
+    'author': 'Odoo S.A.',
+    'depends': ['website'],
+    'data': [ 'views/layout.xml' ],
+    'images': [
+        'static/description/cover.png',
+    ],
+    'application': False,
+}
