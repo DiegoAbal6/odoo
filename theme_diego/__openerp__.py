2b9e6b644e739b39cd9c246624312a3371245571
dabalrodriguez <dabalrodriguez@atanasoft23.danielcastelao.org>
2016-03-02 11:26:43 +0100

    Terminado los services

--- theme_diego/__openerp__.py
+++ theme_diego/__openerp__.py
@@ -6,12 +6,10 @@
     'version': '1.0',
     'author': 'Odoo S.A.',
     'depends': ['website'],
-    'data': [
-  'views/layout.xml',
-  'views/pages.xml'
+    'data': ['views/layout.xml','views/pages.xml','views/assets.xml','views/snippets.xml'],
    
     
-],
+
     'images': [
         'static/description/cover.png',
     ],
