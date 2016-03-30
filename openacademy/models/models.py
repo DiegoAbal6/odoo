f8c31eecee63188865184e67b4b3e8d1029cfd96
dabalrodriguez <dabalrodriguez@atanasoft23.danielcastelao.org>
2016-03-30 11:30:53 +0200

    Preparado para el examen de SXE
    Tema Vacaciones y Modulo finalizado

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -13,58 +13,13 @@ class Course(models.Model):
         'openacademy.session', 'course_id', string="Sesiones")
 
 class Session(models.Model):
-    
-
-  _name = 'openacademy.session'
+    _name = 'openacademy.session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date(default=fields.Date.today)
+    start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    active = fields.Boolean(default=True)    
-    
-    instructor_id = fields.Many2one('res.partner', string="Instructor",
-                                    domain=['|', ('instructor', '=', True),
-                                    ('category_id.name', 'ilike', "Teacher")])
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course',
-                                ondelete='cascade', string="Course", required=True)
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")        
-    
-    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
-
-    @api.depends('seats', 'attendee_ids')
-    def _taken_seats(self):
-        for r in self:
-            if not r.seats:
-                r.taken_seats = 0.0
-            else:
-                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats  
-                
-    @api.onchange('seats', 'attendee_ids')
-    def _verify_valid_seats(self):
-        if self.seats < 0:
-            return {
-                'warning': {
-                    'title': "Incorrect 'seats' value",
-                    'message': "The number of available seats may not be negative",
-                },
-            }
-        if self.seats < len(self.attendee_ids):
-            return {
-                'warning': {
-                    'title': "Too many attendees",
-                    'message': "Increase seats or remove excess attendees",
-                },
-            }
-            
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+        ondelete='cascade', string="Curso", required=True)
---
+++ theme_diego/__init__.py
@@ -0,0 +1,14 @@
+{
+  'name':'Theme_diego',
+  'description': ' Odoo',
+  'version':'1.0',
+  'author':'Diego Abal',
+
+ 'data': [
+  'views/layout.xml',
+  'views/pages.xml',
+  'views/vacaciones.xml',
+  'views/assets.xml'
+],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
